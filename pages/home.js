7bf0d34144564ff31e03b604d77295b8369aa3b5
roderikmogot <roderikmogot17@gmail.com>
2022-07-25 13:38:37 +0800

    added design for non mobile for projects

--- pages/home.js
+++ pages/home.js
@@ -13,7 +13,7 @@ import {
   SiPandas,
   SiNextdotjs,
   SiGithub,
-  SiFigma
+  SiFigma,
 } from "react-icons/si";
 
 export default function Home() {
@@ -64,28 +64,28 @@ export default function Home() {
         </div>
         <div className="p-8 hidden lg:grid lg:grid-cols-3 lg:gap-20 lg:mx-auto lg:w-full lg:h-full">
           <div className="flex items-center justify-center">
-            <SiTensorflow className="text-8xl text-slate-400" />
+            <SiTensorflow className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
           <div className="flex items-center justify-center">
-            <SiNumpy className="text-8xl text-slate-400" />
+            <SiNumpy className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
           <div className="flex items-center justify-center">
-            <SiScikitlearn className="text-8xl text-slate-400" />
+            <SiScikitlearn className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
           <div className="flex items-center justify-center">
-            <SiPandas className="text-8xl text-slate-400" />
+            <SiPandas className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
           <div className="flex items-center justify-center">
-            <SiReact className="text-8xl text-slate-400" />
+            <SiReact className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
           <div className="flex items-center justify-center">
-            <SiNextdotjs className="text-8xl text-slate-400" />
+            <SiNextdotjs className="text-8xl opacity-25 transition duration-100 hover:opacity-80" />
           </div>
         </div>
       </section>
 
       {/* Experiences */}
-      <section className="w-auto mt-4 mx-4 py-4 bg-experiences text-black flex flex-col lg:flex-row lg:justify-center lg:w-auto lg:px-20 lg:py-8 lg:mx-12 lg:h-96">
+      <section className="w-auto mt-4 mx-4 py-4 bg-experiences text-black flex flex-col lg:flex-row lg:justify-center lg:w-auto lg:px-20 lg:py-8 lg:mx-12 lg:h-96 lg:gap-12">
         <div className="p-4 leading-relaxed font-bold text-4xl lg:text-8xl lg:max-w-2xl lg:leading-8 lg:flex lg:justify-center lg:items-center">
           Experiences.
         </div>
@@ -113,26 +113,42 @@ export default function Home() {
       </section>
 
       {/* Projects */}
-      <section className="w-auto mt-4 mx-4 py-4 text-white bg-black flex flex-col lg:flex-row lg:justify-center lg:w-auto lg:px-20 lg:py-8 lg:mx-12 lg:h-96">
+      <section className="w-auto mt-4 mx-4 py-4 text-white bg-black flex flex-col lg:flex-row lg:justify-between lg:w-auto lg:px-20 lg:py-8 lg:mx-12 lg:h-96 lg:gap-12">
         <div className="p-4 leading-relaxed font-bold text-4xl lg:text-8xl lg:max-w-2xl lg:leading-8 lg:flex lg:justify-center lg:items-center">
           Projects.
         </div>
-        <div className="flex flex-row items-center mx-4 gap-4">
-          <div className="flex w-36 h-auto lg:w-24 lg:h-24 lg:relative">
-            <Image
-              src={Sukaido}
-              className="lg:object-fill lg:bg-cover"
-              alt="Sukaido"
-            />
-          </div>
-          <div className="flex flex-col">
-            <div className="font-bold text-xl">Sukaido</div>
-            <div className="text-sm italic text-slate-400">A Mini Restaurant App</div>
-            <div className="flex flex-row gap-2 mt-2">
-            <button className="flex justify-center items-center gap-2 font-bold text-sm bg-primary text-blue-400 p-2 rounded-md transition ease-in-out duration-150 hover:shadow-blue-400 hover:shadow-md">Github<span className="inline-block"><SiGithub /></span></button>
-            <button className="flex justify-center items-center gap-2 font-bold text-sm bg-blue-700 text-red-400 p-2 rounded-md transition ease-in-out duration-150 hover:shadow-red-400 hover:shadow-md">Figma<span className="inline-block"><SiFigma /></span></button>
+        <div className="lg:grid lg:grid-cols-3 lg:gap-4 lg:w-full lg:content-evenly">
+          {/* <div></div> */}
+          <div className="flex flex-row items-center mx-4 gap-4 lg:flex-col lg:justify-center">
+            <div className="flex w-36 h-auto lg:w-48 lg:h-24 lg:relative">
+              <Image
+                src={Sukaido}
+                className="lg:bg-white lg:object-fill lg:bg-cover"
+                alt="Sukaido"
+              />
+            </div>
+            <div className="flex flex-col lg:justify-center lg:items-center">
+              <div className="font-bold text-xl lg:text-2xl">Sukaido</div>
+              <div className="text-sm italic text-slate-400 md:text-md">
+                A Mini Restaurant App
+              </div>
+              <div className="flex flex-row gap-2 mt-2">
+                <button className="flex justify-center items-center gap-2 font-bold text-sm bg-primary text-blue-400 p-2 rounded-md transition ease-in-out duration-150 hover:shadow-blue-400 hover:shadow-md lg:text-md lg:p-3">
+                  Github
+                  <span className="inline-block">
+                    <SiGithub />
+                  </span>
+                </button>
+                <button className="flex justify-center items-center gap-2 font-bold text-sm bg-blue-700 text-red-400 p-2 rounded-md transition ease-in-out duration-150 hover:shadow-blue-400 hover:shadow-md lg:text-md lg:p-3">
+                  Figma
+                  <span className="inline-block">
+                    <SiFigma />
+                  </span>
+                </button>
+              </div>
             </div>
           </div>
+          {/* <div></div> */}
         </div>
       </section>
     </>
