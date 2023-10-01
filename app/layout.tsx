4dac752f5b88b1d9d0e8856ed06ad1794b0dc262
roderikmogot <roderikmogot17@gmail.com>
2023-10-01 15:41:48 +0700

    chore: init 2.0.0

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Roderik Mogot",
+  description:
+    "Roderik Mogot is a software engineer based in Jakarta, Indonesia.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
